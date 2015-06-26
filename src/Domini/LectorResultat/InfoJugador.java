1a41b0daf759b4cfa56f0e9ba1e9cb4f0df48802
jose <sam92.sp@gmail.com>
2015-06-27 00:24:46 +0200

    LectorResultat funcionant

--- src/Domini/LectorResultat/InfoJugador.java
+++ src/Domini/LectorResultat/InfoJugador.java
@@ -1,23 +1,32 @@
 package Domini.LectorResultat;
 
+import java.util.Comparator;
+
 public class InfoJugador {
-        private String nom;
-        private int puntuacio;
-        
-        public InfoJugador(String n, int p){
-            nom = n;
-            puntuacio = p;
+    private String nom;
+    private int puntuacio;
+
+    public InfoJugador(String n, int p){
+        nom = n;
+        puntuacio = p;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public int getPuntuacio(){
+        return puntuacio;
+    }
+
+    public String toString(){
+            return nom + ": " + String.valueOf(puntuacio);
+    }
+
+    public static Comparator<InfoJugador> PUNTUACIO = new Comparator<InfoJugador>() {
+        @Override
+        public int compare(InfoJugador o1, InfoJugador o2) {
+            return Integer.valueOf(o2.puntuacio).compareTo(o1.puntuacio);
         }
-        
-        public String getNom(){
-            return nom;
-        }
-        
-        public int getPuntuacio(){
-            return puntuacio;
-        }
-        
-        public String toString(){
-        	return nom + ": " + String.valueOf(puntuacio);
-        }
+    };
 }
--- src/Domini/LectorResultat/LectorResultat.java
+++ src/Domini/LectorResultat/LectorResultat.java
@@ -3,22 +3,48 @@ package Domini.LectorResultat;
 import java.io.File;
 
 import Domini.Model.Resultat;
+import Presentacio.Controladors.ControladorPrincipal;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LectorResultat {
+    
+    public static void main(String[] args) throws IOException {
+        LectorResultat lr = new LectorResultat();
+        File f = new File("/home/jose/Documentos/Games/game/game.br");
+        try {
+            InfoResultat ir = lr.Llegir(f);
+            System.out.print(ir.toString());
+        } catch (Exception ex) {
+            Logger.getLogger(LectorResultat.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
+    
 	public InfoResultat Llegir(File f) throws FileNotFoundException, IOException, Exception{
 		BufferedReader r = new BufferedReader(new FileReader(f));
                 String l;
-                while((l = r.readLine()) != null && !l.split(" ")[0].equals("names"));
-                if ((l = r.readLine()) != null) throw new Exception("El fitxer no és un fitxer de Battle Royale vàlid.");
-                while((l = r.readLine()) != null && !l.equals("round 100"));
-                while((l = r.readLine()) != null && !l.split(" ")[0].equals("score"));
-                String[] aux = l.split(" ");
-                //InfoJugador p1 = new InfoJugador();
-		return null;
+                String[] names = new String[0];
+                String[] score = new String[0];
+                while((l = r.readLine()) != null){
+                    String[] aux = l.split(" ");
+                    if(aux[0].equals("names")) names = aux;
+                    if(aux[0].equals("score")) score = aux;
+                }
+                if (names.length==0 || score.length==0)
+                    throw new Exception("El fitxer no és un fitxer de Battle Royale vàlid.");
+                
+                InfoResultat info = new InfoResultat(4);
+                for (int i=1; i<=4; ++i){
+                    InfoJugador inf = new InfoJugador(names[i],Integer.parseInt(score[i]));
+                    info.addInfoJugador(inf);
+                }
+                info.ordenar();
+		return info;
 	}
 }
